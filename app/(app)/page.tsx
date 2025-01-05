4ba5e57581aade382880c6822ab546b017a9a97e
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-06 00:25:09 +0600

    refactor (navbar): breadcrumb added

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="container mx-auto gap-2">
+    <div className="">
     </div>
   )
 }
--- components/app/Navbar/navbar.tsx
+++ components/app/Navbar/navbar.tsx
@@ -1,16 +1,46 @@
+"use client"
+
+import { usePathname } from "next/navigation";
 import { SearchForm } from '@/components/forms/search-form'
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
-import React from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
 export const Navbar = () => {
+  const pathname = usePathname(); // Get the current path
+  const currentPage = pathname
+    .split("/")
+    .filter(Boolean) // Remove empty segments
+    .map(segment => segment.charAt(0).toUpperCase() + segment.slice(1)) // Capitalize each segment
+    .join(" / ");
   return (
     <SidebarInset>
-      <header className="flex h-16 items-center justify-between px-4">
-        <SidebarTrigger className="-ml-1" />
-        <div className="flex items-center">
-          <SearchForm />
-        </div>
-      </header>
-    </SidebarInset>
+        <header className="flex h-14 shrink-0 items-center gap-2">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage className="line-clamp-1">
+                  {currentPage || "Home"}
+                  </BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="ml-auto px-3">
+            <SearchForm />
+          </div>
+        </header>
+      </SidebarInset>
   )
 }
